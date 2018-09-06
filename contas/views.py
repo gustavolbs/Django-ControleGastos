d02929320aad5bf46b70d4d9e532ea9892b8dd97
gustavolbs <gustavo.santos@ccc.ufcg.edu.br>
2018-09-05 22:43:50 -0300

    Commit by gustavolbs at quarta 09/05/18 2018 22:43:50

--- contas/views.py
+++ contas/views.py
@@ -15,7 +15,7 @@ def home(request):
 def nova_transacao(request):
     form = TransacaoForm(request.POST or None)
     if form.is_valid():
-    	if request.user.is_superuser():
+    	if request.user.is_superuser:
         	form.save()
         	return redirect('url_home')
     return render(request, "contas/form.html", {"form": form})
@@ -24,13 +24,13 @@ def update(request, pk):
     transacao = Transacao.objects.get(pk=pk)
     form = TransacaoForm(request.POST or None, instance=transacao)
     if form.is_valid():
-    	if request.user.is_superuser():
+    	if request.user.is_superuser:
     		form.save()
     		return redirect('url_home')
     return render(request, "contas/form.html", {"form": form, "transacao": transacao})
 
 def delete(request, pk):
     transacao = Transacao.objects.get(pk=pk)
-    if request.user.is_superuser():
+    if request.user.is_superuser:
     	transacao.delete()
     return redirect('url_home')
